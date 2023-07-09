b01a6bedfd0a032b26c87d91bc0597afeeab1a95
Smarika Adhikari <smarikaadhikari677@gmail.com>
2023-07-09 16:48:00 +0545

    sunday

--- lib/provider/future_provider.dart
+++ lib/provider/future_provider.dart
@@ -2,49 +2,50 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:testriverpod/model/api.dart';
-// import 'package:riverpod_test/ui/models/suggestion.dart';
-import '../services/api_service.dart';
+import 'package:testriverpod/services/api_service.dart';
 
 final suggestionFutureProvider =
-    FutureProvider.autoDispose<Products>((ref) async {
-  final apiService = ref.watch(apiServiceProvider);
-  return apiService.getProducts();
+    FutureProvider.autoDispose<List<Products>>((ref) async {
+  final ApiService = ref.watch(apiServiceProvider);
+  return ApiService.getProducts();
 });
 
 class FutureProviderPage extends ConsumerWidget {
   const FutureProviderPage({super.key});
 
-  // final Color color;
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final FutureProvider = ref.watch(suggestionFutureProvider);
+    // final index = ref.watch(suggestionFutureProvider);
     return Scaffold(
         appBar: AppBar(
-          // backgroundColor: color,
           title: const Text('Future Provider'),
         ),
         body: Center(
           child: FutureProvider.when(data: (data) {
-            return Column(
-              children: [
-                Text(
-                  data.activity,
-                  style: Theme.of(context).textTheme.headlineMedium,
+            return ListView.builder(
+              itemBuilder: (context, index) => Card(
+                child: Column(
+                  children: [
+                    Text(
+                      data[index].id.toString(),
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    Text(
+                      data[index].description,
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    Text(
+                      data[index].price.toString(),
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    Text(
+                      data[index].title,
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                  ],
                 ),
-                Text(
-                  data.type,
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-                Text(
-                  data.price.toString(),
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-                Text(
-                  data.participants.toString(),
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ],
+              ),
             );
           }, error: (error, _) {
             return Text(error.toString());
